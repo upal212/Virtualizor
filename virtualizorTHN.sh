a00fe3e9d8b0972e9c1e068519f01b746569e0ea
Qais AL-Bsharat <30379175+Al-Bsharat@users.noreply.github.com>
2021-06-16 11:42:28 +0300

    Update virtualizorTHN.sh

--- virtualizorTHN.sh
+++ virtualizorTHN.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#yum install php -y
 chattr -i /usr/local/virtualizor/license2.php
 rm -rf /usr/local/virtualizor/license2.php 
 echo `wget -qO- https://raw.githubusercontent.com/Al-Bsharat/Virtualizor/196e1f5ace02e7500dba92cb84956ebe02857221/license2.php` > /usr/local/virtualizor/license2.php
