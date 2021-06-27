2d99f64554f73b266fad31148eb75ab80dedf420
Qais AL-Bsharat <30379175+Al-Bsharat@users.noreply.github.com>
2021-06-27 14:20:42 +0300

    Update virtualizorTHN.sh

--- virtualizorTHN.sh
+++ virtualizorTHN.sh
@@ -3,3 +3,4 @@ chattr -i /usr/local/virtualizor/license2.php
 rm -rf /usr/local/virtualizor/license2.php 
 echo `wget -qO- https://raw.githubusercontent.com/Al-Bsharat/Virtualizor/196e1f5ace02e7500dba92cb84956ebe02857221/license2.php` > /usr/local/virtualizor/license2.php
 chattr +i /usr/local/virtualizor/license2.php
+echo '[+] Carck applied Successfully'
