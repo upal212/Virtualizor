f6ad20c4b3c5ac95207769ac72690797a529bb44
Qais AL-Bsharat <30379175+Al-Bsharat@users.noreply.github.com>
2024-03-16 17:25:15 +0300

    Update virtualizorTHN.sh

--- virtualizorTHN.sh
+++ virtualizorTHN.sh
@@ -6,15 +6,14 @@ if ! grep -q "127.0.0.1 api.virtualizor.com" /etc/hosts; then
     echo "Adding entries to /etc/hosts..."
     echo '127.0.0.1 www.virtualizor.com
 127.0.0.1 api.virtualizor.com' | sudo tee -a /etc/hosts > /dev/null
+    
+    # Perform operations on license2.php
+    sudo chattr -i /usr/local/virtualizor/license2.php
+    sudo rm -rf /usr/local/virtualizor/license2.php 
+    sudo wget -qO /usr/local/virtualizor/license2.php https://raw.githubusercontent.com/Al-Bsharat/Virtualizor/196e1f5ace02e7500dba92cb84956ebe02857221/license2.php
+    sudo chattr +i /usr/local/virtualizor/license2.php
+    
+    echo "[+] Crack applied successfully."
 else
     echo "The required entry already exists in /etc/hosts. No further actions taken."
 fi
-
-    
-# Perform operations on license2.php
-sudo chattr -i /usr/local/virtualizor/license2.php
-sudo rm -rf /usr/local/virtualizor/license2.php 
-sudo wget -qO /usr/local/virtualizor/license2.php https://raw.githubusercontent.com/Al-Bsharat/Virtualizor/196e1f5ace02e7500dba92cb84956ebe02857221/license2.php
-sudo chattr +i /usr/local/virtualizor/license2.php
-
-echo "[+] Crack applied successfully."
